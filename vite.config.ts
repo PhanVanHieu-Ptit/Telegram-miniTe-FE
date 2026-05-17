01964269ea94b1ce495413132d467d9524762e97
hieu <phanvanhieu090201@gmail.com>
2026-05-17 15:52:28 +0700

    feat: update-ui

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    allowedHosts: [
+      'mollusklike-nonfatally-felicita.ngrok-free.dev'
+    ]
+  }
 })
